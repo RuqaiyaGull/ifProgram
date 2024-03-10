15f510be6c4b7d3e39f29aa95dc2dd3661806079
RuqaiyaGull <162179273+RuqaiyaGull@users.noreply.github.com>
2024-03-10 21:21:33 +0500

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,19 @@
+var percentAge = 45;
+if (percentAge > 90) {
+    console.log("Grade A+");
+}
+else if (percentAge > 80) {
+    console.log("Grade A");
+}
+else if (percentAge > 70) {
+    console.log("Grade B");
+}
+else if (percentAge > 60) {
+    console.log("Grade C");
+}
+else if (percentAge > 50) {
+    console.log("Grade D");
+}
+else {
+    console.log("Fail");
+}
